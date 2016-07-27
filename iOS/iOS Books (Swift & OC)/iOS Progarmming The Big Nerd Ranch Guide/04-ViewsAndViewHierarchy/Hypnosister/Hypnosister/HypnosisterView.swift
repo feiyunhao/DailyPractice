121bb9ed9e0f5b9acacd5ed5cbe173e99e91a798
feiyunhao <hao-shen@qq.com>
2016-07-27 15:03:10 +0800

    视图

--- iOS/iOS Books (Swift & OC)/iOS Progarmming The Big Nerd Ranch Guide/04-ViewsAndViewHierarchy/Hypnosister/Hypnosister/HypnosisterView.swift
+++ iOS/iOS Books (Swift & OC)/iOS Progarmming The Big Nerd Ranch Guide/04-ViewsAndViewHierarchy/Hypnosister/Hypnosister/HypnosisterView.swift
@@ -21,7 +21,6 @@ class HypnosisterView: UIView {
         let rect = self.bounds
         let center = CGPointMake(rect.origin.x + rect.size.width / 2,
                                  rect.origin.y + rect.size.height / 2)
-        
         let maxRadius = hypot(center.x, center.y)/2
         let bezierPath = UIBezierPath()
         
@@ -30,41 +29,53 @@ class HypnosisterView: UIView {
             bezierPath.moveToPoint(CGPointMake(center.x + currentRadius, center.y))
             bezierPath.addArcWithCenter(center, radius: currentRadius, startAngle: 0, endAngle: CGFloat(M_PI * 2), clockwise: true)
         }
-        
         bezierPath.lineWidth = 10
         UIColor.lightGrayColor().setStroke()        
         bezierPath.stroke()
         
-        let logoImage = UIImage(named: "logo")
-        logoImage?.drawInRect(CGRectInset(self.bounds, 50, 100))
-        
         
         let contextRef = UIGraphicsGetCurrentContext()
+        
+        CGContextSaveGState(contextRef)
+        CGContextSetShadow(contextRef, CGSizeMake(4, 7), 3)
+        
+        let logoImage = UIImage(named: "logo")
+        logoImage?.drawInRect(CGRectInset(self.bounds, 80, 180))
+        
+        CGContextRestoreGState(contextRef)
+        
+        
         CGContextSetRGBStrokeColor(contextRef, 1, 0, 0, 1)
         let path = CGPathCreateMutable()
         CGPathMoveToPoint(path, nil, 0, 100)
-        CGPathAddLineToPoint(path, nil, 300, 100)
+        CGPathAddLineToPoint(path, nil, 414, 100)
         CGContextAddPath(contextRef, path)
         CGContextStrokePath(contextRef)
         
         UIColor.blueColor().setStroke()
         let path2 = UIBezierPath()
         path2.moveToPoint(CGPointMake(0, 105))
-        path2.addLineToPoint(CGPointMake(300, 105))
+        path2.addLineToPoint(CGPointMake(414, 105))
         path2.stroke()
         
-        let gradient = CGGradientCreateWithColors(CGColorSpaceCreateDeviceRGB(), [UIColor.redColor(),UIColor.yellowColor(),UIColor.blueColor()], [0.3,0.7])
-        
-        CGContextDrawLinearGradient(contextRef, gradient, CGPointZero, CGPointMake(1, 1), [.DrawsAfterEndLocation,.DrawsBeforeStartLocation])
         
         CGContextSaveGState(contextRef)
+        let path3 = UIBezierPath()
+        path3.addClip()
+        
+        path3.moveToPoint(CGPointMake(200, 0))
+        path3.addLineToPoint(CGPointMake(0, 700))
+        path3.addLineToPoint(CGPointMake(414, 700))
+        path3.closePath()
+        path3.addClip()//“设置为当前图形上下文currentContext的剪切路径”
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let gradient = CGGradientCreateWithColorComponents(colorSpace, [0,1,0,0,0,0,1,1], [0,1], 2)
+        
+        CGContextDrawLinearGradient(contextRef, gradient, CGPointZero, CGPointMake(414, 732), [])
         CGContextRestoreGState(contextRef)
         
-        
-        
-        
-        
     }
-    
 
 }
